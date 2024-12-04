85570f022a0eeb1df0af4a9f453c519fcb5d1aeb
waseem817 <s.shoaibwaseem786@gmail.com>
2024-12-04 13:33:31 +0530

    Add files via upload

---
+++ multiplicationOf4OrNot.java
@@ -0,0 +1,15 @@
+class multiplicationOf4OrNot
+{
+    public static void main(String[] args)
+    {
+        int a=5;
+        if((a&3)==0)
+        {
+            System.out.println("is a multiple of 4");
+        }
+        else
+        {
+            System.out.println("is not a multiple of 4");
+        }
+    }
+}
