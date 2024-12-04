34e8e81f5614bda06b84e7b844d442d5f481e4d5
waseem817 <s.shoaibwaseem786@gmail.com>
2024-12-04 10:59:17 +0530

    Add files via upload

---
+++ SetbitOrNot.java
@@ -0,0 +1,16 @@
+class SetbitOrNot
+{
+    public static void main(String[] args)
+    {
+        int a=5;
+        int pos=1;
+        if((a&(1<<pos))!=0)
+        {
+            System.out.println("is a set bit");
+        }
+        else
+        {
+            System.out.println("is a set bit");
+        }
+    }
+}
