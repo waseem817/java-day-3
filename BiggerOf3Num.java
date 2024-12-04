46b9c775da4718cd9737964e3b16b68b9a44c208
waseem817 <s.shoaibwaseem786@gmail.com>
2024-12-04 14:02:34 +0530

    Add files via upload

---
+++ BiggerOf3Num.java
@@ -0,0 +1,21 @@
+class BiggerOf3Num
+{
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=2;
+        int c=8;
+        if((a>b)&&(a>c))
+        {
+            System.out.println("a is bigger");
+        }
+        else if((b>a)&&(b>c))
+        {
+            System.out.println("b is bigger");
+        }
+        else
+        {
+            System.out.println("c is bigger");
+        }
+    }
+}
